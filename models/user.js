3d28e48b84f05a5f4df1ff4c8e0ec0310251cfc9
Haeeul <etoilune00@gmail.com>
2020-07-17 09:17:54 +0900

    fix [POST] /users/signup

--- models/user.js
+++ models/user.js
@@ -2,8 +2,8 @@ const pool = require('../modules/pool');
 const table = 'USER';
 
 exports.signUp = async (data)=> {
-    const fields = 'univ_idx, user_name, user_id, user_password, user_salt, user_point';
-    const query = `INSERT INTO ${table}(${fields}) VALUES(${data.user_university}, '${data.user_name}', '${data.user_id}', '${data.user_hashed}', '${data.user_salt}', 0 )`;
+    const fields = 'univ_idx, user_name, user_id, user_password, user_salt';
+    const query = `INSERT INTO ${table}(${fields}) VALUES(${data.user_university}, '${data.user_name}', '${data.user_id}', '${data.user_hashed}', '${data.user_salt}')`;
     try {
         const result = await pool.queryParam(query);
         return result.insertId;
